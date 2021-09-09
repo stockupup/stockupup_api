c8400c2f6f05bb9f58f729c7cc2cf31d805cf86d
nerJll <2360300702@qq.com>
2021-09-09 21:12:28 +0800

    Update StockContanst.java
    注释

--- src/main/java/com/xxxz/stockupup/v1/contanst/StockContanst.java
+++ src/main/java/com/xxxz/stockupup/v1/contanst/StockContanst.java
@@ -5,10 +5,14 @@ import java.io.Serializable;
 /**
  * @autor jiangll
  * @date 2021/9/9
+ * 全局常量
  */
 public final class StockContanst implements Serializable {
     private static final long serialVersionUID = 1285402108070629579L;
 
+    /**
+     * 初始用户
+     */
     public static final String[] HolderInitData = {
             "豆豆", "强哥", "志军", "鸡哥", "隆兄",
             "靖威", "正正", "猪哥", "涛哥", "老姜"
